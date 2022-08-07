95d908759b643b2350509696fa4d8ea5e0c7e4ef
moran991231 <moran991231@naver.com>
2022-08-07 16:26:19 +0900

    finish main

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { backgrounds } from "../components/backgrounds";
 import "../styles/Main.css";
+import { useNavigate } from "react-router-dom";
 
 export default function Main() {
   let back_img = Number(new Date().getSeconds()) % backgrounds.length;
+  const navigate = useNavigate();
 
   return (
     <div className="main-con">
@@ -15,15 +17,28 @@ export default function Main() {
       />
       <div className="main-front">
         <div className="main-title">Feel-Dok</div>
-        <div className="main-desc">2022/07/28 ~ 2022/07/31 </div>
-        <div className="main-desc">서울에서 울릉도를 거쳐 독도까지 </div>
-        <button className="main-btn-map">관광지도</button>
-        <button className="main-btn-video">유투브 링크</button>
+        <div className="main-desc">
+          2022/07/28 ~ 2022/07/31 서울에서 울릉도를 거쳐 독도까지
+        </div>
+
+        <button className="main-btn main-map" onClick={() => navigate("/info")}>
+          관광지도 바로가기
+        </button>
+        <iframe
+          title="Youtube Video"
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/juQvizeZJFM"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
         <div className="main-footer">
-          Haechan Park, Jaesun Park, Seoyeon Park, Jonghyun Park (not a family)
+          Haechan Park, Jaesun Park, Seoyeon Park, Jonghyeon Park (not a family)
+          (feat. Univ. of Seoul)
         </div>
         <div className="main-footer">
-          made by. Jaesun Park,{" "}
+          made by. Jaesun Park{" "}
           <a href="https://github.com/moran991231">GitHub @moran991231</a>
         </div>
       </div>
